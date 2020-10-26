f92c824787f0d8d4a851a561514bca6f5c19273e
Alexey Otjukh <yoshirolas@tut.by>
2020-10-26 22:45:50 +0300

    refact task model

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -1,55 +1,59 @@
 const uuid = require('uuid');
+const { USE_MONGO } = require('./../../common/config');
 
-const mongoose = require('mongoose');
+class TaskInMemoryModel {
+  constructor({
+    id = uuid(),
+    title = 'BOARD',
+    order = '0',
+    description = 'Test task',
+    userId = null,
+    boardId = null,
+    columnId = null
+  } = {}) {
+    this.id = id;
+    this.title = title;
+    this.order = order;
+    this.description = description;
+    this.userId = userId;
+    this.boardId = boardId;
+    this.columnId = columnId;
+  }
+}
 
-const taskSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: uuid,
-  },
-  title: String,
-  order: Number,
-  description: String,
-  userId: {
-    type: String,
-    default: uuid,
-  },
-  boardId: {
-    type: String,
-    default: uuid,
-  },
-  columnId: {
-    type: String,
-    default: uuid,
-  },
-});
+const getTaskMongoModel = () => {
+  const mongoose = require('mongoose');
 
-taskSchema.statics.toResponse = (task) => {
-  const { _id, title, order, description, userId, boardId, columnId } = task;
-  return { id: _id, title, order, description, userId,  boardId, columnId };
+  const taskSchema = new mongoose.Schema({
+    _id: {
+      type: String,
+      default: uuid
+    },
+    title: String,
+    order: Number,
+    description: String,
+    userId: {
+      type: String,
+      default: uuid
+    },
+    boardId: {
+      type: String,
+      default: uuid
+    },
+    columnId: {
+      type: String,
+      default: uuid
+    }
+  });
+
+  taskSchema.statics.toResponse = task => {
+    const { _id, title, order, description, userId, boardId, columnId } = task;
+    return { id: _id, title, order, description, userId, boardId, columnId };
+  };
+
+  return mongoose.model('Task', taskSchema);
 };
 
-
-const Task = mongoose.model('Task', taskSchema);
-
-// class Task {
-//   constructor({
-//     id = uuid(),
-//     title = 'BOARD',
-//     order = '0',
-//     description = 'Test task',
-//     userId = null,
-//     boardId = null,
-//     columnId = null
-//   } = {}) {
-//     this.id = id;
-//     this.title = title;
-//     this.order = order;
-//     this.description = description;
-//     this.userId = userId;
-//     this.boardId = boardId;
-//     this.columnId = columnId;
-//   }
-// }
+const Task = USE_MONGO ? getTaskMongoModel() : TaskInMemoryModel;
 
 module.exports = Task;
