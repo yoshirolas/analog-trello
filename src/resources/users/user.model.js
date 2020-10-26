99f2e2322ca713db6be21679a4e7eb82ae74042c
Alexey Otjukh <yoshirolas@tut.by>
2020-10-26 22:32:09 +0300

    refact user model

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,42 +1,46 @@
 const uuid = require('uuid');
-const mongoose = require('mongoose');
+const { USE_MONGO } = require('./../../common/config');
 
-const userSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: uuid,
-  },
-  name: String,
-  login: String,
-  password: String,
-});
+class UserInMemory {
+  constructor({
+    id = uuid(),
+    name = 'USER',
+    login = 'user',
+    password = 'P@55w0rd'
+  } = {}) {
+    this.id = id;
+    this.name = name;
+    this.login = login;
+    this.password = password;
+  }
 
-userSchema.statics.toResponse = (user) => {
-  const { _id, name, login } = user;
-  return { id: _id, name, login };
+  static toResponse(user) {
+    const { id, name, login } = user;
+    return { id, name, login };
+  }
+}
+
+const getUserMongoModel = () => {
+  const mongoose = require('mongoose');
+
+  const userSchema = new mongoose.Schema({
+    _id: {
+      type: String,
+      default: uuid
+    },
+    name: String,
+    login: String,
+    password: String
+  });
+
+  userSchema.statics.toResponse = user => {
+    const { _id, name, login } = user;
+    return { id: _id, name, login };
+  };
+
+  return mongoose.model('User', userSchema);
 };
 
-const User = mongoose.model('User', userSchema);
-
-// class User {
-//   constructor({
-//     id = uuid(),
-//     name = 'USER',
-//     login = 'user',
-//     password = 'P@55w0rd'
-//   } = {}) {
-//     this.id = id;
-//     this.name = name;
-//     this.login = login;
-//     this.password = password;
-//   }
-
-//   static toResponse(user) {
-//     const { id, name, login } = user;
-//     return { id, name, login };
-//   }
-// }
-
-
+const User = USE_MONGO ? getUserMongoModel() : UserInMemory;
 
 module.exports = User;
