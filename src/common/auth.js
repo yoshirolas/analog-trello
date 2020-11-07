a36f2659da86e2ba94982d7c6896e26838755341
Alexey Otjukh <yoshirolas@tut.by>
2020-11-07 14:24:14 +0300

    hotfix: skipping auth urls

--- src/common/auth.js
+++ src/common/auth.js
@@ -1,12 +1,15 @@
 const createErr = require('http-errors');
 const { verifyToken } = require('./jwt');
 
-const SKIP_AUTH_URLS = ['/doc', '/login', '/'];
+const ENTRY_POINT_URL = '/';
+const SKIP_AUTH_URLS = ['/doc/', '/login'];
 
 const checkAuth = async (req, res, next) => {
   try {
     const url = req.originalUrl;
-    if (SKIP_AUTH_URLS.indexOf(url) > -1) return next(); // Skip
+    for (const skipUrl of SKIP_AUTH_URLS) {
+      if (url.indexOf(skipUrl) > -1 || url === ENTRY_POINT_URL) return next(); // Skip
+    }
     const authHeader = req.headers.authorization;
     if (!authHeader) throw new createErr(401, 'Unauthorized error');
 
